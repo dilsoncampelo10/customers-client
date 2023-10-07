9bc05fdd0acb281d324fe3234b5666e5d938fab3
dilsoncampelo10 <dilsono316@gmail.com>
2023-10-07 14:46:32 -0300

    customer model

---
+++ src/app/models/Customer.ts
@@ -0,0 +1,15 @@
+export class Customer{
+    id: number = 0;
+    name: string = '';
+    cpf: string = '';
+    rg: string = '';
+    birthdate: string = '';
+    phone: string = '';
+    street: string = '';
+    number_address: string = '';
+    zip_code: string = '';
+    neighborhood: string = '';
+    city : string = '';
+    complement : string = '';
+    email : string = '';
+}
